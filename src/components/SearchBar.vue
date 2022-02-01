f3931cb68d2d906b75aec4b5b6f35886ba1e865b
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-02-01 12:48:45 +0100

    Fix Indentation SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -7,7 +7,9 @@
         v-model="keywordSearch"
         @keyup.enter="$emit('search', keywordSearch.toLowerCase())"
       />
-      <button @click="$emit('search', keywordSearch.toLowerCase())">Search</button>
+      <button @click="$emit('search', keywordSearch.toLowerCase())">
+        Search
+      </button>
       <button @click="reset()">Reset</button>
     </div>
   </div>
@@ -21,30 +23,30 @@ export default {
     }
   },
   methods: {
-      reset(){
-          this.$emit('reset');
-          this.keywordSearch='';
-      }
+    reset() {
+      this.$emit('reset')
+      this.keywordSearch = ''
+    },
   },
 }
 </script>
 <style lang="scss" scoped>
-.filter{
-    display: flex;
-    padding-top:20px ;
+.filter {
+  display: flex;
+  padding-top: 20px;
 
-    input{
-        width: 80%;
-        height: 50px;
-        line-height: 50px;
-        padding-left:20px ;
-    }
-    button{
-        width: calc(10% - 20px);
-        margin-left:20px;
-        background-color: #333;
-        color: #FFF;
-        border-radius: 10px;
-    }
+  input {
+    width: 80%;
+    height: 50px;
+    line-height: 50px;
+    padding-left: 20px;
+  }
+  button {
+    width: calc(10% - 20px);
+    margin-left: 20px;
+    background-color: #333;
+    color: #fff;
+    border-radius: 10px;
+  }
 }
 </style>
